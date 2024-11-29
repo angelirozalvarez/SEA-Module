0fc4fed5a4b29921bdbc7df7fb1cac88126a5499
angelirozalvarez <alvarezangeliroz40@gmail.com>
2024-11-29 16:09:37 +0000

    Make compatible

--- trades/models.py
+++ trades/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from __future__ import annotations
 
 class Trader(models.Model):
     first_name = models.CharField(max_length=30)
